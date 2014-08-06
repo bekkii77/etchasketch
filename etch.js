8db33eb56efcf2eb0cc2810e7a5447ac8a38eb52
Bekki Freeman <bekki@tinymission.com>
2014-08-06 09:08:08 -0500

    Add features for random color, greyscale, trail

--- etch.js
+++ etch.js
@@ -1,29 +1,22 @@
+var dimension = 50;
+var option = "change"; //change, random, darken, trail
+
 $(document).ready(function() {
 
-	var dimension = 16;
 	drawGrid();
 
-	function drawGrid () {
-		container = $(".container"); 
-		console.log("draw grid " + dimension);		
-		squareWidth = 512 / dimension;
-		container.append("<div class='row'></div>");
-		row = container.children(":first");
-		for (var i = 1; i <= dimension*dimension; i++) {
-			row.append("<div class='tile'></div>");
-			if (i%dimension == 0) {
-				container.append("<div class='row'></div>");
-				row = container.children(":last");
-			};
-		};
-		$(".tile").css({width: squareWidth, height: squareWidth}).hover(function() {
-			$(this).addClass("passed");
-		});
-	}
+	$("#clear").click(function() {
+		if (option === "change") {
+			$(".tile").removeClass('passed');
+		}
+		else {
+			$(".tile").css({"background-color":"white", "opacity":1});
+		}
+	});
 
-	$("#clear").on("click", function() {
-		$(".tile").removeClass('passed');
-		var newDim = prompt("Please enter the desired width.");
+	$("#change").on("click", function() {
+		option = "change";
+		var newDim = prompt("Please enter the desired width.", 50);
 		if (newDim != null) {
 			dimension = newDim;
 			console.log("new dim " + dimension);
@@ -32,4 +25,92 @@ $(document).ready(function() {
 		}
 	});
 
+	$("#greyscale").on("click", function() {
+		option = "darken";
+		var newDim = prompt("Please enter the desired width.", 50);
+		if (newDim != null) {
+			dimension = newDim;
+			console.log("new dim " + dimension);
+			$(".container").empty();
+			drawGrid();
+		}
+	});
+
+	$("#random").on("click", function() {
+		option = "random";
+		var newDim = prompt("Please enter the desired width.", 50);
+		if (newDim != null) {
+			dimension = newDim;
+			console.log("new dim " + dimension);
+			$(".container").empty();
+			drawGrid();
+		}
+	});
+
+	$("#trail").on("click", function() {
+		option = "trail";
+		var newDim = prompt("Please enter the desired width.", 50);
+		if (newDim != null) {
+			dimension = newDim;
+			console.log("new dim " + dimension);
+			$(".container").empty();
+			drawGrid();
+		}
+	});
 });
+
+function drawGrid () {
+	container = $(".container"); 
+	console.log("draw grid " + dimension);		
+	squareWidth = container.width() / dimension;
+	for (var i = 1; i <= dimension*dimension; i++) {
+		container.append("<div class='tile'></div>");
+	};
+	$(".tile").css({width: squareWidth, height: squareWidth});
+
+	if(option === "change") {
+		$(".tile").mouseenter(function() {
+			$(this).addClass("passed");
+		});
+	}
+	else if (option === "darken") {
+		$(".tile").mouseenter(function() {
+			$(this).css("background-color", darkenColor($(this).css("background-color")));
+		});
+	}
+	else if (option === "random") {
+		$(".tile").mouseenter(function() {
+			$(this).css("background-color", randomColor());
+		});
+	}
+	else if (option === "trail") {
+		$(".tile").hover(function() {
+			$(this).css("opacity", 0);
+		}, function() {
+			$(this).fadeTo('slow', 1);
+		});
+	}
+}
+
+function darkenColor(rgb){
+ 	rgb = rgb.match(/^rgb\((\d+),\s*(\d+),\s*(\d+)\)$/);
+ 	var red=parseInt(rgb[1]);
+ 	var green=parseInt(rgb[2]);
+ 	var blue=parseInt(rgb[3]);
+
+ 	red = red - 50;
+ 	green = green - 50;
+ 	blue = blue - 50;
+ 	newRgb = "rgb(" + Math.floor(red) + "," + Math.floor(green) + "," + Math.floor(blue) + ")";
+ 	// console.log(newRgb);
+ 	return newRgb;
+ };
+
+function randomColor() {
+	red = Math.floor(Math.random()*255);
+	green = Math.floor(Math.random()*255);
+	blue = Math.floor(Math.random()*255);
+ 	newRgb = "rgb(" + Math.floor(red) + "," + Math.floor(green) + "," + Math.floor(blue) + ")";
+ 	console.log(newRgb);
+ 	return newRgb;
+}
